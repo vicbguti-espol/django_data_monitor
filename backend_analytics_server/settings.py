dfdc48fc6d41816881b20b75bdbd085da00f292f
Cliente CONTROL-PC <CltControl@GGIECLUGWRK164.espol.edu.ec>
2025-08-18 10:29:21 -0500

    25

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
+
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,8 +79,12 @@ WSGI_APPLICATION = "backend_analytics_server.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ.get('MYSQLDATABASE'),
+        'USER': os.environ.get('MYSQLUSER'),
+        'PASSWORD': os.environ.get('MYSQLPASSWORD'),
+        'HOST': os.environ.get('MYSQLHOST'),
+        'PORT': os.environ.get('MYSQLPORT'),
     }
 }
 
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class DashboardModel(models.Model):
+
+    class Meta:
+        permissions = [
+            ("index_viewer", "Can show to index view (function-based)"),
+        ]
